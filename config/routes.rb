42f974efa06c35fe21369a8e004a0bfec85d8f63
James Sral <jamessral@gmail.com>
2016-02-24 17:31:59 +0000

    Complete Authentication System

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
