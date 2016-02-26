30f6df51b8543d65eb7d991719acfd466ebe5ad9
James Sral <jamessral@gmail.com>
2016-02-26 12:03:54 -0500

    Update 20160126174030_add_description_to_articles.rb

--- db/migrate/20160126174030_add_description_to_articles.rb
+++ db/migrate/20160126174030_add_description_to_articles.rb
@@ -1,6 +1,5 @@
 class AddDescriptionToArticles < ActiveRecord::Migration
   def change
-    add_column :articles, :description, :text
     add_column :articles, :created_at, :datetime
     add_column :articles, :updated_at, :datetime
   end
