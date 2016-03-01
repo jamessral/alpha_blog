916312bc59a587604a94d749a1d249edeb7610dc
James Sral <jamessral@gmail.com>
2016-03-01 18:07:48 +0000

    Fix migration issue with categories table

--- db/migrate/20160228195132_add_timestamps_to_categories.rb
+++ db/migrate/20160228195132_add_timestamps_to_categories.rb
@@ -1,6 +1,5 @@
 class AddTimestampsToCategories < ActiveRecord::Migration
   def change
-    add_column :categories, :created_at, :datetime
     add_column :categories, :updated_at, :datetime
   end
 end
